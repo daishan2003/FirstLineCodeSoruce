8f793c791420735f87d2a59e3034ea1dab4aab8b
lp-pp <1049345904@qq.com>
2017-10-17 10:40:28 +0800

    init commit

---
+++ CoolWeather/src/main/java/com/lp/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,8 @@
+package com.lp.coolweather;
+
+/**
+ * Created by LP on 2017/10/16/19:20.
+ */
+
+public class ChooseAreaFragment {
+}
--- CoolWeather/src/main/java/com/lp/coolweather/MainActivity.java
+++ CoolWeather/src/main/java/com/lp/coolweather/MainActivity.java
@@ -1,7 +1,13 @@
-package com.lp.firstlinecodesoruce;
+package com.lp.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lp.firstlinecodesoruce.R;
+
+/**
+ * Created by LP on 2017/10/16/19:20
+ */
 
 public class MainActivity extends AppCompatActivity {
 
